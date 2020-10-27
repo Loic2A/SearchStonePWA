93026f8914d38189dd5c82adfa83365e978b0a29
JohnSmoothie <vicoboix@gmail.com>
2020-10-27 17:03:34 +0100

    Test de l'utilisation d'Hearthstone API

--- back/getCarte.js
+++ back/getCarte.js
@@ -1,5 +1,19 @@
 
+
 $(document).ready(function () {
-    $( "p" ).text( '<b>Some</b> new text.' );
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards?locale=frFR",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+            "x-rapidapi-key": "acc93a5516msh1444a5fc4c180adp1013c1jsn4e04544b762f"
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 });
 
