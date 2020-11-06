c711884f897ac4f8dbc0c1639f180d84abdfff79
Loic2A <lomedori.15@gmail.com>
2020-11-06 19:45:30 +0100

    Modifs architecture test

--- front/searchstone-pwa/src/components/Body.js
+++
@@ -1,7 +0,0 @@
-const Body = props => (
-    <div>
-        <p className="App-intro">{props.text}</p>
-        <p className="App-intro">{props.text2}</p>
-        <p className="App-intro">{props.myFunc(1, 2)}</_p>
-    </div>
-)
