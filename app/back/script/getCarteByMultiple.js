ec5a05ca28c72d8aff39bc5f8b1d5b0091944f15
theoc2a <theocussac2a@gmail.com>
2020-11-07 20:11:49 +0100

    modif de getCarteByMultiple

--- app/back/script/getCarteByMultiple.js
+++ app/back/script/getCarteByMultiple.js
@@ -1,5 +1,48 @@
 import { transformUrlImage } from "/back/script/transformUrlImage.js";
 //fait une requete sur l'api à partir d'un critère puis filtre le resultat obtenus selon les autres critères
 export function getCarteByMultiple(args) {
-
+    if (args[0] != null) {
+        Response = getCarteByClass(args.classes);
+        $.each(Response){
+            if (Response.sets == args.sets & Response.types == args.types & Response.factions == args.factions & Response.qualities == args.qualities & Response.races == args.races)
+                Response1 = Response;
+        }
+        return Response1;
+    }
+    else if (args[1] != null) {
+        Response = getCarteBySet(args.sets);
+        $.each(Response){
+            if (Response.types == args.types & Response.factions == args.factions & Response.qualities == args.qualities & Response.races == args.races)
+                Response1 = Response;
+        }
+        return Response1;
+    }
+    else if (args[2] != null) {
+        Response = getCarteByType(args.types);
+        $.each(Response){
+            if (Response.factions == args.factions & Response.qualities == args.qualities & Response.races == args.races)
+                Response1 = Response;
+        }
+        return Response1;
+    }
+    else if (args[3] != null) {
+        Response = getCarteByFaction(args.factions);
+        $.each(Response){
+            if (Response.qualities == args.qualities & Response.races == args.races)
+                Response1 = Response;
+        }
+        return Response1;
+    }
+    else if (args[4] != null) {
+        Response = getCarteByQuality(args.qualities);
+        $.each(Response){
+            if (Response.races == args.races)
+                Response1 = Response;
+        }
+        return Response1;
+    }
+    else if (args[5] != null) {
+        Response = getCarteByRace(args.races);
+        return Response;
+    }
 }
