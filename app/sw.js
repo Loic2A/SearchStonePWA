d0505cb6798c5926f3c4a2123e2a6371136a5074
JohnSmoothie <vicoboix@gmail.com>
2020-11-11 16:11:38 +0100

    ajout du offline

--- app/sw.js
+++ app/sw.js
@@ -1,2 +1,6 @@
 self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>SearchStone a besoin de la connexion Internet. Veuillez vous connecter</div>', headers));
+    }
 });
